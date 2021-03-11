25332254f36f93e3a65365101667419b085fc4ee
bilek993 <jbilinskipl@gmail.com>
2021-03-11 20:01:59 +0100

    Added logger files without content.

---
+++ engine/common/debug/Logger.h
@@ -0,0 +1,8 @@
+//
+// Created by jbili on 11.03.2021.
+//
+
+#ifndef CLUSEK_RT_LOGGER_H
+#define CLUSEK_RT_LOGGER_H
+
+#endif //CLUSEK_RT_LOGGER_H
---
+++ engine/common/debug/LoggerModes.h
@@ -0,0 +1,8 @@
+//
+// Created by jbili on 11.03.2021.
+//
+
+#ifndef CLUSEK_RT_LOGGERMODES_H
+#define CLUSEK_RT_LOGGERMODES_H
+
+#endif //CLUSEK_RT_LOGGERMODES_H
