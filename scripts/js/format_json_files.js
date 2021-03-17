fe83524c60e0e3a6a830eb3537aaeaba09791b53
bilek993 <jbilinskipl@gmail.com>
2021-03-17 16:16:29 +0100

    Added formatter JS script.

---
+++ scripts/js/format_json_files.js
@@ -0,0 +1,29 @@
+const { resolve } = require('path');
+const { readdir } = require('fs').promises;
+const { readFileSync, writeFileSync } = require('fs');
+
+async function* getFiles(dir) {
+  const dirents = await readdir(dir, { withFileTypes: true });
+  for (const dirent of dirents) {
+    const res = resolve(dir, dirent.name);
+    if (dirent.isDirectory()) {
+      yield* getFiles(res);
+    } else {
+      yield res;
+    }
+  }
+}
+
+(async () => {
+    const searchPath = process.argv[2] ?? '.';
+
+    for await (const filePath of getFiles(searchPath)) {
+      if (filePath.toLowerCase().endsWith('.json')) {
+          console.log("Formatting '" + filePath + ' file...');
+
+          let data = readFileSync(filePath);
+          let formattedData = JSON.stringify(JSON.parse(data), null, 3);
+          writeFileSync(filePath, formattedData);
+      }
+    }
+  })()
