c120b16141ae28721a1bf6cdd62495ec6019fb8c
bilek993 <jbilinskipl@gmail.com>
2021-03-19 16:37:43 +0100

    Added scaffold files for ConfigData and ConfigMacros.

---
+++ engine/configuration/ConfigData.h
@@ -0,0 +1,13 @@
+//
+// Created by jbili on 19.03.2021.
+//
+
+#ifndef CLUSEK_RT_CONFIGDATA_H
+#define CLUSEK_RT_CONFIGDATA_H
+
+struct ConfigData
+{
+
+};
+
+#endif //CLUSEK_RT_CONFIGDATA_H
---
+++ engine/configuration/ConfigMacros.h
@@ -0,0 +1,8 @@
+//
+// Created by jbili on 19.03.2021.
+//
+
+#ifndef CLUSEK_RT_CONFIGMACROS_H
+#define CLUSEK_RT_CONFIGMACROS_H
+
+#endif //CLUSEK_RT_CONFIGMACROS_H
