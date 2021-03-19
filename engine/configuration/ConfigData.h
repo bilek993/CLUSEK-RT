642be6d33364ab05311dadac291b3d588bd36189
bilek993 <jbilinskipl@gmail.com>
2021-03-19 16:54:51 +0100

    Added two macros and first field in the ConfigData.

--- engine/configuration/ConfigData.h
+++ engine/configuration/ConfigData.h
@@ -5,9 +5,13 @@
 #ifndef CLUSEK_RT_CONFIGDATA_H
 #define CLUSEK_RT_CONFIGDATA_H
 
+#include "ConfigMacros.h"
+
+CONFIG_OBJECT(NAME = ConfigData)
 struct ConfigData
 {
-
+    CONFIG_PARAMETER(NAME = FilePath, TYPE = std::string)
+    std::string FilePath{};
 };
 
 #endif //CLUSEK_RT_CONFIGDATA_H
