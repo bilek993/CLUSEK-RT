d26116a2da17ae14b9d2cb406c061e052f1506c3
bilek993 <jbilinskipl@gmail.com>
2021-03-20 14:24:35 +0100

    Added replacing all.

--- scripts/js/user_modules/standarization.js
+++ scripts/js/user_modules/standarization.js
@@ -2,7 +2,7 @@ STANDARIZED_SPLIT_CHARACTER = '/';
 STANDARIZED_NEW_LINE_CHARACTER = '\n';
 
 function standarizePath(path) {
-    return path.replace('\\', '/');
+    return path.replace(/\\/g, '/');
 }
 
 module.exports = { STANDARIZED_SPLIT_CHARACTER, STANDARIZED_NEW_LINE_CHARACTER, standarizePath }
