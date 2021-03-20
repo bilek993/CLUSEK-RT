ef686311a526c0a7da61db0e7de9fed1f6eb6e72
bilek993 <jbilinskipl@gmail.com>
2021-03-20 15:09:22 +0100

    Added template for generating header.

--- scripts/js/generate_config_parser.js
+++ scripts/js/generate_config_parser.js
@@ -1,7 +1,9 @@
 const { getFiles } = require('./user_modules/files_helper');
 const { standarizePath, STANDARIZED_SPLIT_CHARACTER } = require('./user_modules/standarization');
 const { readFileSync, writeFileSync } = require('fs');
+const mustache = require('mustache');
 
+const TEMPLATE_HEADER_PATH = './templates/ConfigGeneratorTemplate.h';
 const SKIP_FILE_DECORATOR = 'GENERATOR_SKIP_THIS_FILE';
 const CONFIG_FILE_DECORATOR = 'CONFIG_OBJECT';
 
@@ -10,7 +12,10 @@ function checkIfIsConfigFile(data) {
 }
 
 function generateData(data) {
-  return '// TODO: Add generator implementation';
+  template = readFileSync(TEMPLATE_HEADER_PATH).toString();
+  view = {};
+
+  return mustache.render(template, view);
 }
 
 function generateOutputPath(filePath, generatorPath) {
