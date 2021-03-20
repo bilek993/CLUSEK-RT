d435cf2080424794900d063c3fc6bbf3de7f16b1
bilek993 <jbilinskipl@gmail.com>
2021-03-20 14:29:23 +0100

    Added logic for generating parser header files from JS.

--- scripts/js/generate_config_parser.js
+++ scripts/js/generate_config_parser.js
@@ -1,16 +1,25 @@
 const { getFiles } = require('./user_modules/files_helper');
+const { standarizePath, STANDARIZED_SPLIT_CHARACTER } = require('./user_modules/standarization');
 const { readFileSync, writeFileSync } = require('fs');
 
-function checkIfIsConfigFile(data) {
+const SKIP_FILE_DECORATOR = 'GENERATOR_SKIP_THIS_FILE';
+const CONFIG_FILE_DECORATOR = 'CONFIG_OBJECT';
 
+function checkIfIsConfigFile(data) {
+  return data.includes(CONFIG_FILE_DECORATOR) && !data.includes(SKIP_FILE_DECORATOR);
 }
 
 function generateData(data) {
-
+  return '// TODO: Add generator implementation';
 }
 
-function generateOutputPath(path) {
+function generateOutputPath(filePath, generatorPath) {
+  let standarizedFilePath = standarizePath(filePath);
+  let splittedStandarizedFilePath = standarizedFilePath.split(STANDARIZED_SPLIT_CHARACTER);
+  
+  let standarizedGeneratorPath = standarizePath(generatorPath);
 
+  return standarizedGeneratorPath + splittedStandarizedFilePath[splittedStandarizedFilePath.length - 1];
 }
 
 (async () => {
@@ -24,12 +33,18 @@ function generateOutputPath(path) {
           let data = readFileSync(filePath);
 
           if (checkIfIsConfigFile(data)) {
+              console.log("Found config file at path '" + filePath + "'...");
+
               let generatedData = generateData(data);
-              let generatedPath = generateOutputPath(filePath);
+              let generatedPath = generateOutputPath(filePath, generatorPath);
 
               writeFileSync(generatedPath, generatedData);
+
+              console.log("Generated new file '" + generatedPath + "' with parser!");
           }
       }
     }
-  })()
+
+    console.log('Finishing config generator...');
+  })();
   
