3c8fa3bf56e50ba1996992b0bdd5dad9ca59d586
bilek993 <jbilinskipl@gmail.com>
2021-03-20 10:36:05 +0100

    Moved hardcoded values into consts in two JS scripts.

--- scripts/js/format_json_files.js
+++ scripts/js/format_json_files.js
@@ -1,13 +1,15 @@
 const { getFiles } = require('./user_modules/files_helper');
 const { readFileSync, writeFileSync } = require('fs');
 
+const SEARCHED_EXTENSTION = '.json';
+
 (async () => {
     console.log('Starting formatter...');
 
     const searchPath = process.argv[2] ?? '.';
 
     for await (const filePath of getFiles(searchPath)) {
-      if (filePath.toLowerCase().endsWith('.json')) {
+      if (filePath.toLowerCase().endsWith(SEARCHED_EXTENSTION)) {
           console.log("Formatting '" + filePath + ' file...');
 
           let data = readFileSync(filePath);
--- scripts/js/generate_ascii_text.js
+++ scripts/js/generate_ascii_text.js
@@ -1,5 +1,8 @@
 const art = require('ascii-art');
 
+const DEFAULT_FONT = 'Doom';
+const SPLIT_CHAR = '\n';
+
 if (process.argv.length < 3) {
     console.error('\x1b[31m%s\x1b[0m', 'Failed!');
     console.error('\x1b[31m%s\x1b[0m', 'Please provide correct parameters!!!');
@@ -17,8 +20,8 @@ const inputString = process.argv[2].replace(' ', '   ');
 const opening = process.argv[3] ?? '';
 const closing = process.argv[4] ?? '';
 
-art.font(inputString, 'Doom', function (error, rendered) {
-    let splittedText = rendered.split('\n');
+art.font(inputString, DEFAULT_FONT, function (error, rendered) {
+    let splittedText = rendered.split(SPLIT_CHAR);
     splittedText.forEach((value) => {
         if (value.trim().length != 0) {
             console.log(opening + value + closing);
