43e89e964f85144dad2ddf0cef2c0a57cb9deb42
bilek993 <jbilinskipl@gmail.com>
2021-03-20 10:21:25 +0100

    Added scaffold for generating config parser.

--- scripts/js/generate_config_parser.js
+++ scripts/js/generate_config_parser.js
@@ -1,1 +1,35 @@
-console.log('TO BE IMPLEMENTED');
+const { getFiles } = require('./user_modules/files_helper');
+const { readFileSync, writeFileSync } = require('fs');
+
+function checkIfIsConfigFile(data) {
+
+}
+
+function generateData(data) {
+
+}
+
+function generateOutputPath(path) {
+
+}
+
+(async () => {
+    console.log('Starting config generator...');
+
+    const generatorPath = process.argv[2];
+    const searchPath = process.argv[3];
+
+    for await (const filePath of getFiles(searchPath)) {
+      if (filePath.toLowerCase().endsWith('.h')) {
+          let data = readFileSync(filePath);
+
+          if (checkIfIsConfigFile(data)) {
+              let generatedData = generateData(data);
+              let generatedPath = generateOutputPath(filePath);
+
+              writeFileSync(generatedPath, generatedData);
+          }
+      }
+    }
+  })()
+  
