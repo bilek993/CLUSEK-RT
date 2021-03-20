f367a7854e4d21c7e530c789b105f09da1425ad6
bilek993 <jbilinskipl@gmail.com>
2021-03-20 12:23:05 +0100

    Fixed standarization function.

--- scripts/js/user_modules/standarization.js
+++ scripts/js/user_modules/standarization.js
@@ -2,7 +2,7 @@ STANDARIZED_SPLIT_CHARACTER = '/';
 STANDARIZED_NEW_LINE_CHARACTER = '\n';
 
 function standarizePath(path) {
-    return filePath.replace('\\', '/');
+    return path.replace('\\', '/');
 }
 
 module.exports = { STANDARIZED_SPLIT_CHARACTER, STANDARIZED_NEW_LINE_CHARACTER, standarizePath }
