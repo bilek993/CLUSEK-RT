85a0a61f7cf17f15129eedda48931c0ae642b3f3
bilek993 <jbilinskipl@gmail.com>
2021-03-21 17:07:21 +0100

    Changed default split character.

--- scripts/js/user_modules/standarization.js
+++ scripts/js/user_modules/standarization.js
@@ -1,8 +1,8 @@
-STANDARIZED_SPLIT_CHARACTER = '/';
+STANDARIZED_SPLIT_CHARACTER = '\\';
 STANDARIZED_NEW_LINE_CHARACTER = '\n';
 
 function standarizePath(path) {
-    return path.replace(/\\/g, '/');
+    return path.replace(/\//g, '\\');
 }
 
 module.exports = { STANDARIZED_SPLIT_CHARACTER, STANDARIZED_NEW_LINE_CHARACTER, standarizePath }
