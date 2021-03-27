42f0a565886e3405a22be3bf7ae8748f91ba51ea
bilek993 <jbilinskipl@gmail.com>
2021-03-27 09:18:56 +0100

    Added first version of archive JS tool.

---
+++ scripts/js/redistribute_engine.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+const archiver = require('archiver');
+
+console.log('Preparing CLUSEK-RT to redistribution...');
+
+if (process.argv.length < 3) {
+    console.error('\x1b[31m%s\x1b[0m', 'Failed! Path to bin folder not provided!');
+    return;
+}
+
+const binPath = process.argv[2];
+const projectVersion = process.argv[3] ?? '1.0.0';
+const projectName = process.argv[4] ?? 'CLUSEK-RT';
+
+const output = fs.createWriteStream(projectName + '-' + projectVersion + '.zip');
+const archive = archiver('zip', {
+    zlib: { level: 9 }
+});
+
+archive.on('warning', (err) => console.log(err));
+archive.on('error', (err) => { throw err });
+output.on('close', () => console.log('Finished with saving ' + archive.pointer() + ' total bytes.'));
+
+archive.pipe(output);
+
+archive.directory(binPath, false);
+
+archive.finalize();
