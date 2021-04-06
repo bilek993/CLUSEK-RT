53fbe170f72e5e1e5e8b38f5215cbef581b5d4bd
bilek993 <jbilinskipl@gmail.com>
2021-04-06 20:41:23 +0200

    Added proper logging in Vulkan Callback.

--- engine/renderer/helpers/DebuggerCallback.cpp
+++ engine/renderer/helpers/DebuggerCallback.cpp
@@ -11,7 +11,21 @@ VkBool32 DebuggerCallback::Callback(VkDebugUtilsMessageSeverityFlagBitsEXT messa
                                     const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
                                     void* pUserData)
 {
-    LOG_DEBUG("aaaaaaaaaaaaaaaaaa"); // TODO: Change this
+    switch (messageSeverity)
+    {
+    case VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT:
+    case VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT:
+        LOG_DEBUG(pCallbackData->pMessage);
+        break;
+    case VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT:
+        LOG_WARNING(pCallbackData->pMessage);
+        break;
+    case VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT:
+    case VK_DEBUG_UTILS_MESSAGE_SEVERITY_FLAG_BITS_MAX_ENUM_EXT:
+        LOG_ERROR(pCallbackData->pMessage);
+        break;
+    }
+
     return VK_FALSE;
 }
 
