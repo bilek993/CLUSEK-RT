be753ddea47eaca544b5a7d71a44480918aa78e0
bilek993 <jbilinskipl@gmail.com>
2021-04-07 17:44:34 +0200

    Added empty class for logical device.

---
+++ engine/renderer/core/VulkanLogicalDevice.h
@@ -0,0 +1,15 @@
+//
+// Created by jbili on 07.04.2021.
+//
+
+#ifndef CLUSEK_RT_VULKANLOGICALDEVICE_H
+#define CLUSEK_RT_VULKANLOGICALDEVICE_H
+
+
+class VulkanLogicalDevice
+{
+
+};
+
+
+#endif //CLUSEK_RT_VULKANLOGICALDEVICE_H
