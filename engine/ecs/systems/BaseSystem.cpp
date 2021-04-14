ca2664df52ca54d3ded3d31a6edbe365e0e50fab
bilek993 <jbilinskipl@gmail.com>
2021-04-14 18:02:19 +0200

    Converted systems vector into weak pointer.

--- engine/ecs/systems/BaseSystem.cpp
+++ engine/ecs/systems/BaseSystem.cpp
@@ -13,7 +13,7 @@
 
 void BaseSystem::Start(std::shared_ptr<ConfigData> configurationData,
                        std::shared_ptr<Window> mainWindow,
-                       std::shared_ptr<std::vector<std::shared_ptr<BaseSystem>>> systems)
+                       std::weak_ptr<std::vector<std::shared_ptr<BaseSystem>>> systems)
 {
     LOG_DEBUG(fmt::format(FMT_COMPILE("Starting '{}'..."), GetName()));
 
