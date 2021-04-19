8543c4231cddb80d676ebeed0329296832b86fe0
bilek993 <jbilinskipl@gmail.com>
2021-04-19 18:12:31 +0200

    Added logic for allocating queues.

--- engine/renderer/core/VulkanQueues.cpp
+++ engine/renderer/core/VulkanQueues.cpp
@@ -3,3 +3,74 @@
 //
 
 #include "VulkanQueues.h"
+
+#include <algorithm>
+
+VulkanQueues::VulkanQueues(const std::shared_ptr<VulkanPhysicalDevice> physicalDevice,
+                           const unsigned int graphicsQueuesCount,
+                           const unsigned int computeQueuesCount,
+                           const unsigned int transferQueues)
+{
+    auto graphicsQueuesToBeAllocated = graphicsQueuesCount;
+    auto computeQueuesToBeAllocated = computeQueuesCount;
+    auto transferQueuesToBeAllocated = transferQueues;
+
+    const auto queueFamilies = GetAllQueueFamilyProperties(physicalDevice);
+
+    for (auto i = 0; i < queueFamilies.size(); i++)
+    {
+        const auto& queueFamily = queueFamilies[i];
+        auto currentFamilyLeftQueues = queueFamily.queueCount;
+
+        if (graphicsQueuesToBeAllocated > 0 && queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT)
+        {
+            const auto allocatedQueues = std::min(graphicsQueuesToBeAllocated, currentFamilyLeftQueues);
+            currentFamilyLeftQueues -= allocatedQueues;
+
+            for (auto j = 0; j < allocatedQueues; j++)
+            {
+                VulkanQueue graphicQueue{};
+                graphicQueue.FamilyIndex = i;
+                GraphicsQueues.emplace_back(graphicQueue);
+            }
+        }
+
+        if (computeQueuesToBeAllocated > 0 && queueFamily.queueFlags & VK_QUEUE_COMPUTE_BIT)
+        {
+            const auto allocatedQueues = std::min(computeQueuesToBeAllocated, currentFamilyLeftQueues);
+            currentFamilyLeftQueues -= allocatedQueues;
+
+            for (auto j = 0; j < allocatedQueues; j++)
+            {
+                VulkanQueue computeQueue{};
+                computeQueue.FamilyIndex = i;
+                ComputeQueues.emplace_back(computeQueue);
+            }
+        }
+
+        if (transferQueuesToBeAllocated > 0 && queueFamily.queueFlags & VK_QUEUE_TRANSFER_BIT)
+        {
+            const auto allocatedQueues = std::min(transferQueuesToBeAllocated, currentFamilyLeftQueues);
+            currentFamilyLeftQueues -= allocatedQueues;
+
+            for (auto j = 0; j < allocatedQueues; j++)
+            {
+                VulkanQueue transferQueue{};
+                transferQueue.FamilyIndex = i;
+                TransferQueues.emplace_back(transferQueue);
+            }
+        }
+    }
+}
+
+std::vector<VkQueueFamilyProperties>
+VulkanQueues::GetAllQueueFamilyProperties(const std::shared_ptr<VulkanPhysicalDevice> physicalDevice)
+{
+    uint32_t queueFamilyCount = 0;
+    vkGetPhysicalDeviceQueueFamilyProperties(physicalDevice->GetRaw(), &queueFamilyCount, nullptr);
+
+    std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+    vkGetPhysicalDeviceQueueFamilyProperties(physicalDevice->GetRaw(), &queueFamilyCount, queueFamilies.data());
+
+    return queueFamilies;
+}
--- engine/renderer/core/VulkanQueues.h
+++ engine/renderer/core/VulkanQueues.h
@@ -5,18 +5,31 @@
 #ifndef CLUSEK_RT_VULKANQUEUES_H
 #define CLUSEK_RT_VULKANQUEUES_H
 
+#include <vulkan/vulkan.h>
+#include <vector>
+
+#include "VulkanQueue.h"
+#include "VulkanPhysicalDevice.h"
 
 class VulkanQueues final
 {
 public:
-    VulkanQueues() = default;
+    VulkanQueues(std::shared_ptr<VulkanPhysicalDevice> physicalDevice,
+                 unsigned int graphicsQueuesCount,
+                 unsigned int computeQueuesCount,
+                 unsigned int transferQueuesCount);
     ~VulkanQueues() = default;
     VulkanQueues(const VulkanQueues& other) = delete;
     VulkanQueues(VulkanQueues&& other) noexcept = delete;
     VulkanQueues& operator=(const VulkanQueues& other) = delete;
     VulkanQueues& operator=(VulkanQueues&& other) noexcept = delete;
-
 private:
+    static std::vector<VkQueueFamilyProperties>
+    GetAllQueueFamilyProperties(std::shared_ptr<VulkanPhysicalDevice> physicalDevice);
+
+    std::vector<VulkanQueue> GraphicsQueues{};
+    std::vector<VulkanQueue> ComputeQueues{};
+    std::vector<VulkanQueue> TransferQueues{};
 };
 
 
