5e24b826c22dd450559bb9daa9bea5124b6c4c7a
bilek993 <jbilinskipl@gmail.com>
2021-04-20 20:04:52 +0200

    Prepeared for creating debug extender functions.

---
+++ engine/renderer/helpers/DebugExtender.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jbili on 20.04.2021.
+//
+
+#include "DebugExtender.h"
---
+++ engine/renderer/helpers/DebugExtender.h
@@ -0,0 +1,15 @@
+//
+// Created by jbili on 20.04.2021.
+//
+
+#ifndef CLUSEK_RT_DEBUGEXTENDER_H
+#define CLUSEK_RT_DEBUGEXTENDER_H
+
+
+namespace DebugExtender
+{
+    
+};
+
+
+#endif //CLUSEK_RT_DEBUGEXTENDER_H
