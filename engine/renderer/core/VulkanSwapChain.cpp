2a8fe9df028d6b5a4df833d9b91de9cc2055a6e2
bilek993 <jbilinskipl@gmail.com>
2021-05-05 14:53:03 +0200

    Added swapchain class with basic logic (checking support for formats and selecting presentation mode).

---
+++ engine/renderer/core/VulkanSwapChain.cpp
@@ -0,0 +1,79 @@
+//
+// Created by jbili on 05.05.2021.
+//
+
+#include "VulkanSwapChain.h"
+
+#include <algorithm>
+
+VulkanSwapChain::VulkanSwapChain(std::shared_ptr<VulkanLogicalDevice> logicalDevice,
+                                 const std::shared_ptr<VulkanPhysicalDevice> physicalDevice,
+                                 const std::shared_ptr<VulkanSurface> surface,
+                                 const VkSurfaceFormatKHR requestedFormat,
+                                 const std::vector<VkPresentModeKHR>& requestedPresentationModes)
+{
+    LogicalDevice = std::move(logicalDevice);
+
+    const auto isFormatSupported = CheckRequestedFormatSupport(surface, physicalDevice, requestedFormat);
+    if (isFormatSupported)
+        throw std::runtime_error("Requested Swap Chain format is not supported!");
+}
+
+VulkanSwapChain::~VulkanSwapChain()
+{
+    vkDestroySwapchainKHR(LogicalDevice->GetRaw(), InternalSwapchain, nullptr);
+}
+
+bool VulkanSwapChain::CheckRequestedFormatSupport(const std::shared_ptr<VulkanSurface> surface,
+                                                  const std::shared_ptr<VulkanPhysicalDevice> physicalDevice,
+                                                  const VkSurfaceFormatKHR requestedFormat)
+{
+    uint32_t supportedFormatsCount;
+    auto result = vkGetPhysicalDeviceSurfaceFormatsKHR(physicalDevice->GetRaw(),
+                                                       surface->GetRaw(),
+                                                       &supportedFormatsCount,
+                                                       nullptr);
+    if (result != VK_SUCCESS)
+        throw std::runtime_error("Critical error when getting surface formats!");
+
+    std::vector<VkSurfaceFormatKHR> supportedFormats{ supportedFormatsCount };
+    result = vkGetPhysicalDeviceSurfaceFormatsKHR(physicalDevice->GetRaw(),
+                                                  surface->GetRaw(),
+                                                  &supportedFormatsCount,
+                                                  supportedFormats.data());
+    if (result != VK_SUCCESS)
+        throw std::runtime_error("Critical error when getting surface formats!");
+
+
+    return std::any_of(supportedFormats.cbegin(), supportedFormats.cend(), [&requestedFormat](VkSurfaceFormatKHR format)
+    {
+        return format.format == requestedFormat.format && format.colorSpace && requestedFormat.colorSpace;
+    });
+}
+
+VkPresentModeKHR VulkanSwapChain::SelectPresentationMode(const std::shared_ptr<VulkanPhysicalDevice> physicalDevice,
+                                                         const std::shared_ptr<VulkanSurface> surface,
+                                                         const std::vector<VkPresentModeKHR>& requestedPresentationModes)
+{
+    uint32_t supportedPresentModesCount;
+    auto result = vkGetPhysicalDeviceSurfacePresentModesKHR(physicalDevice->GetRaw(),
+                                                            surface->GetRaw(),
+                                                            &supportedPresentModesCount,
+                                                            nullptr);
+    if (result != VK_SUCCESS)
+        throw std::runtime_error("Critical error when getting surface presentation modes!");
+
+    std::vector<VkPresentModeKHR> supportedPresentModes{ supportedPresentModesCount };
+    result = vkGetPhysicalDeviceSurfacePresentModesKHR(physicalDevice->GetRaw(),
+                                                       surface->GetRaw(),
+                                                       &supportedPresentModesCount,
+                                                       supportedPresentModes.data());
+    if (result != VK_SUCCESS)
+        throw std::runtime_error("Critical error when getting surface presentation modes!");
+
+    for (auto requestedPresentationMode : requestedPresentationModes)
+        for (auto supportedPresentMode : supportedPresentModes)
+            if (requestedPresentationMode == supportedPresentMode) return requestedPresentationMode;
+
+    throw std::runtime_error("Found 0 compatible presentation modes (based on requested values)!");
+}
---
+++ engine/renderer/core/VulkanSwapChain.h
@@ -0,0 +1,42 @@
+//
+// Created by jbili on 05.05.2021.
+//
+
+#ifndef CLUSEK_RT_VULKANSWAPCHAIN_H
+#define CLUSEK_RT_VULKANSWAPCHAIN_H
+
+#include <vulkan/vulkan.h>
+#include <memory>
+
+#include "VulkanLogicalDevice.h"
+#include "VulkanPhysicalDevice.h"
+#include "VulkanSurface.h"
+
+class VulkanSwapChain
+{
+public:
+    VulkanSwapChain(std::shared_ptr<VulkanLogicalDevice> logicalDevice,
+                    std::shared_ptr<VulkanPhysicalDevice> physicalDevice,
+                    std::shared_ptr<VulkanSurface> surface,
+                    VkSurfaceFormatKHR requestedFormat,
+                    const std::vector<VkPresentModeKHR>& requestedPresentationModes);
+    ~VulkanSwapChain();
+    VulkanSwapChain(const VulkanSwapChain& other) = delete;
+    VulkanSwapChain(VulkanSwapChain&& other) noexcept = delete;
+    VulkanSwapChain& operator=(const VulkanSwapChain& other) = delete;
+    VulkanSwapChain& operator=(VulkanSwapChain&& other) noexcept = delete;
+
+private:
+    static bool CheckRequestedFormatSupport(std::shared_ptr<VulkanSurface> surface,
+                                            std::shared_ptr<VulkanPhysicalDevice> physicalDevice,
+                                            VkSurfaceFormatKHR requestedFormat);
+    static VkPresentModeKHR SelectPresentationMode(std::shared_ptr<VulkanPhysicalDevice> physicalDevice,
+                                                   std::shared_ptr<VulkanSurface> surface,
+                                                   const std::vector<VkPresentModeKHR>& requestedPresentationModes);
+
+    VkSwapchainKHR InternalSwapchain = VK_NULL_HANDLE;
+    std::shared_ptr<VulkanLogicalDevice> LogicalDevice;
+};
+
+
+#endif //CLUSEK_RT_VULKANSWAPCHAIN_H
