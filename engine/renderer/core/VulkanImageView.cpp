bc088438b3166d9b8b1655112a222f43d9a9ceb6
bilek993 <jbilinskipl@gmail.com>
2021-05-13 15:09:36 +0200

    Created ImageView logic for creation and destruction.

--- engine/renderer/core/VulkanImageView.cpp
+++ engine/renderer/core/VulkanImageView.cpp
@@ -3,3 +3,45 @@
 //
 
 #include "VulkanImageView.h"
+
+VulkanImageView::VulkanImageView(std::shared_ptr<VulkanLogicalDevice> logicalDevice,
+                                 const std::shared_ptr<VulkanImage> image,
+                                 const VkImageViewType imageViewType,
+                                 const VkFormat format,
+                                 const VkImageAspectFlags aspectMask,
+                                 const uint32_t baseMipLevel,
+                                 const uint32_t levelCount,
+                                 const uint32_t baseArrayLayer,
+                                 const uint32_t layerCount)
+{
+    LogicalDevice = std::move(logicalDevice);
+
+    VkImageViewCreateInfo imageViewCreateInfo{};
+    imageViewCreateInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+    imageViewCreateInfo.image = image->GetRaw();
+    imageViewCreateInfo.viewType = imageViewType;
+    imageViewCreateInfo.format = format;
+    imageViewCreateInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+    imageViewCreateInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+    imageViewCreateInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+    imageViewCreateInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+    imageViewCreateInfo.subresourceRange.aspectMask = aspectMask;
+    imageViewCreateInfo.subresourceRange.baseMipLevel = baseMipLevel;
+    imageViewCreateInfo.subresourceRange.levelCount = levelCount;
+    imageViewCreateInfo.subresourceRange.baseArrayLayer = baseArrayLayer;
+    imageViewCreateInfo.subresourceRange.layerCount = layerCount;
+
+    const auto result = vkCreateImageView(LogicalDevice->GetRaw(), &imageViewCreateInfo, nullptr, &InternalImageView);
+    if (result != VK_SUCCESS)
+        throw std::runtime_error("Image view creation failed!");
+}
+
+VulkanImageView::~VulkanImageView()
+{
+    vkDestroyImageView(LogicalDevice->GetRaw(), InternalImageView, nullptr);
+}
+
+VkImageView VulkanImageView::GetRaw() const
+{
+    return InternalImageView;
+}
--- engine/renderer/core/VulkanImageView.h
+++ engine/renderer/core/VulkanImageView.h
@@ -5,10 +5,30 @@
 #ifndef CLUSEK_RT_VULKANIMAGEVIEW_H
 #define CLUSEK_RT_VULKANIMAGEVIEW_H
 
+#include <vulkan/vulkan.h>
+#include <memory>
+
+#include "VulkanLogicalDevice.h"
+#include "VulkanImage.h"
 
 class VulkanImageView final
 {
+public:
+    VulkanImageView(std::shared_ptr<VulkanLogicalDevice> logicalDevice,
+                    std::shared_ptr<VulkanImage> image,
+                    VkImageViewType imageViewType,
+                    VkFormat format,
+                    VkImageAspectFlags aspectMask,
+                    uint32_t baseMipLevel,
+                    uint32_t levelCount,
+                    uint32_t baseArrayLayer,
+                    uint32_t layerCount);
+    virtual ~VulkanImageView();
 
+    [[nodiscard]] VkImageView GetRaw() const;
+private:
+    VkImageView InternalImageView = VK_NULL_HANDLE;
+    std::shared_ptr<VulkanLogicalDevice> LogicalDevice;
 };
 
 
