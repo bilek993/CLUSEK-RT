dfe1f75a31f880c15610fec4e24110cac0fd326b
bilek993 <jbilinskipl@gmail.com>
2021-05-24 19:58:26 +0200

    Implemented command pool logic.

--- engine/renderer/core/CommandPool.cpp
+++ engine/renderer/core/CommandPool.cpp
@@ -3,3 +3,38 @@
 //
 
 #include "CommandPool.h"
+
+CommandPool::CommandPool(std::shared_ptr<VulkanLogicalDevice> logicalDevice,
+                         const VulkanQueue& queue,
+                         const bool shortLivedCommandBuffers,
+                         const bool resetCommandBuffersIndividually)
+{
+    LogicalDevice = std::move(logicalDevice);
+
+    VkCommandPoolCreateInfo commandPoolCreateInfo{};
+    commandPoolCreateInfo.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
+    commandPoolCreateInfo.queueFamilyIndex = queue.GetFamilyIndex();
+    commandPoolCreateInfo.flags = 0;
+
+    if (shortLivedCommandBuffers)
+        commandPoolCreateInfo.flags |= VK_COMMAND_POOL_CREATE_TRANSIENT_BIT;
+    if (resetCommandBuffersIndividually)
+        commandPoolCreateInfo.flags |= VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT;
+
+    const auto result = vkCreateCommandPool(LogicalDevice->GetRaw(),
+                                            &commandPoolCreateInfo,
+                                            nullptr,
+                                            &InternalCommandPool);
+    if (result != VK_SUCCESS)
+        throw std::runtime_error("Command pool cannot be created!");
+}
+
+CommandPool::~CommandPool()
+{
+    vkDestroyCommandPool(LogicalDevice->GetRaw(), InternalCommandPool, nullptr);
+}
+
+VkCommandPool CommandPool::GetRaw() const
+{
+    return InternalCommandPool;
+}
--- engine/renderer/core/CommandPool.h
+++ engine/renderer/core/CommandPool.h
@@ -5,10 +5,30 @@
 #ifndef CLUSEK_RT_COMMANDPOOL_H
 #define CLUSEK_RT_COMMANDPOOL_H
 
+#include <vulkan/vulkan.h>
+
+#include "VulkanQueue.h"
+#include "VulkanLogicalDevice.h"
 
 class CommandPool final
 {
+public:
+    CommandPool(std::shared_ptr<VulkanLogicalDevice> logicalDevice,
+                const VulkanQueue& queue,
+                bool shortLivedCommandBuffers,
+                bool resetCommandBuffersIndividually);
+    ~CommandPool();
+    CommandPool(const CommandPool& other) = delete;
+    CommandPool(CommandPool&& other) noexcept = delete;
+    CommandPool& operator=(const CommandPool& other) = delete;
+    CommandPool& operator=(CommandPool&& other) noexcept = delete;
 
+    [[nodiscard]] VkCommandPool GetRaw() const;
+
+private:
+    VkCommandPool InternalCommandPool = VK_NULL_HANDLE;
+
+    std::shared_ptr<VulkanLogicalDevice> LogicalDevice;
 };
 
 
