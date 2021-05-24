ab0d6f83429a115f526ac521adc0f1a971557d6c
bilek993 <jbilinskipl@gmail.com>
2021-05-24 19:40:57 +0200

    Added empty command pool class.

---
+++ engine/renderer/core/CommandPool.h
@@ -0,0 +1,15 @@
+//
+// Created by jbili on 24.05.2021.
+//
+
+#ifndef CLUSEK_RT_COMMANDPOOL_H
+#define CLUSEK_RT_COMMANDPOOL_H
+
+
+class CommandPool final
+{
+
+};
+
+
+#endif //CLUSEK_RT_COMMANDPOOL_H
