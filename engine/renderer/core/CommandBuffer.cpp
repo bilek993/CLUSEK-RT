455f1f3014c516072d15408ab6ef098b5eacad8c
bilek993 <jbilinskipl@gmail.com>
2021-05-24 19:40:11 +0200

    Added empty command buffer class.

---
+++ engine/renderer/core/CommandBuffer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jbili on 24.05.2021.
+//
+
+#include "CommandBuffer.h"
---
+++ engine/renderer/core/CommandBuffer.h
@@ -0,0 +1,15 @@
+//
+// Created by jbili on 24.05.2021.
+//
+
+#ifndef CLUSEK_RT_COMMANDBUFFER_H
+#define CLUSEK_RT_COMMANDBUFFER_H
+
+
+class CommandBuffer final
+{
+
+};
+
+
+#endif //CLUSEK_RT_COMMANDBUFFER_H
