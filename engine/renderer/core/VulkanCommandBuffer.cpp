8bd13b6d09f5f9b97c8705e62c977e4a04e45759
bilek993 <jbilinskipl@gmail.com>
2021-05-25 18:39:39 +0200

    Added logic for creating and destroying command buffer.

--- engine/renderer/core/VulkanCommandBuffer.cpp
+++ engine/renderer/core/VulkanCommandBuffer.cpp
@@ -3,3 +3,34 @@
 //
 
 #include "VulkanCommandBuffer.h"
+
+VulkanCommandBuffer::VulkanCommandBuffer(std::shared_ptr<VulkanLogicalDevice> logicalDevice,
+                                         std::shared_ptr<VulkanCommandPool> commandPool,
+                                         VkCommandBufferLevel level)
+{
+    LogicalDevice = std::move(logicalDevice);
+    CommandPool = std::move(commandPool);
+
+    VkCommandBufferAllocateInfo commandBufferAllocateInfo{};
+    commandBufferAllocateInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+    commandBufferAllocateInfo.commandPool = CommandPool->GetRaw();
+    commandBufferAllocateInfo.level = level;
+    commandBufferAllocateInfo.commandBufferCount = 1;
+
+    const auto result = vkAllocateCommandBuffers(LogicalDevice->GetRaw(),
+                                                 &commandBufferAllocateInfo,
+                                                 &InternalCommandBuffer);
+    if (result != VK_SUCCESS)
+        throw std::runtime_error("Command buffer cannot be created!");
+}
+
+VulkanCommandBuffer::~VulkanCommandBuffer()
+{
+    if (InternalCommandBuffer != VK_NULL_HANDLE)
+        vkFreeCommandBuffers(LogicalDevice->GetRaw(), CommandPool->GetRaw(), 1, &InternalCommandBuffer);
+}
+
+VkCommandBuffer VulkanCommandBuffer::GetRaw() const
+{
+    return InternalCommandBuffer;
+}
--- engine/renderer/core/VulkanCommandBuffer.h
+++ engine/renderer/core/VulkanCommandBuffer.h
@@ -5,10 +5,30 @@
 #ifndef CLUSEK_RT_VULKANCOMMANDBUFFER_H
 #define CLUSEK_RT_VULKANCOMMANDBUFFER_H
 
+#include <vulkan/vulkan.h>
+
+#include "VulkanLogicalDevice.h"
+#include "VulkanCommandPool.h"
 
 class VulkanCommandBuffer final
 {
+public:
+    VulkanCommandBuffer(std::shared_ptr<VulkanLogicalDevice> logicalDevice,
+                        std::shared_ptr<VulkanCommandPool> commandPool,
+                        VkCommandBufferLevel level);
+    ~VulkanCommandBuffer();
+    VulkanCommandBuffer(const VulkanCommandBuffer& other) = delete;
+    VulkanCommandBuffer(VulkanCommandBuffer&& other) noexcept = delete;
+    VulkanCommandBuffer& operator=(const VulkanCommandBuffer& other) = delete;
+    VulkanCommandBuffer& operator=(VulkanCommandBuffer&& other) noexcept = delete;
 
+    [[nodiscard]] VkCommandBuffer GetRaw() const;
+
+private:
+    VkCommandBuffer InternalCommandBuffer = VK_NULL_HANDLE;
+
+    std::shared_ptr<VulkanLogicalDevice> LogicalDevice;
+    std::shared_ptr<VulkanCommandPool> CommandPool;
 };
 
 
