3db3c4e8c808d5e3a13886fda1bd7831cb099716
bilek993 <jbilinskipl@gmail.com>
2021-05-31 21:13:06 +0200

    Added empty VulkanSemaphore class.

---
+++ engine/renderer/core/VulkanSemaphore.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jbili on 28.05.2021.
+//
+
+#include "VulkanSemaphore.h"
---
+++ engine/renderer/core/VulkanSemaphore.h
@@ -0,0 +1,15 @@
+//
+// Created by jbili on 28.05.2021.
+//
+
+#ifndef CLUSEK_RT_VULKANSEMAPHORE_H
+#define CLUSEK_RT_VULKANSEMAPHORE_H
+
+
+class VulkanSemaphore final
+{
+
+};
+
+
+#endif //CLUSEK_RT_VULKANSEMAPHORE_H
