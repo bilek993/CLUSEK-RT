a1d29de858da2538416bc87606a799c8f07867cc
bilek993 <jbilinskipl@gmail.com>
2021-05-31 21:26:59 +0200

    Added empty VulkanFence class.

---
+++ engine/renderer/core/VulkanFence.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jbili on 31.05.2021.
+//
+
+#include "VulkanFence.h"
---
+++ engine/renderer/core/VulkanFence.h
@@ -0,0 +1,14 @@
+//
+// Created by jbili on 31.05.2021.
+//
+
+#ifndef CLUSEK_RT_VULKANFENCE_H
+#define CLUSEK_RT_VULKANFENCE_H
+
+class VulkanFence final
+{
+
+};
+
+
+#endif //CLUSEK_RT_VULKANFENCE_H
