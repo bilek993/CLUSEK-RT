60312ce69517f0b5cbb1f6827887f8a6eff08634
bilek993 <jbilinskipl@gmail.com>
2021-06-01 15:01:57 +0200

    Implemented Vulkan Fence.

--- engine/renderer/core/VulkanFence.cpp
+++ engine/renderer/core/VulkanFence.cpp
@@ -3,3 +3,25 @@
 //
 
 #include "VulkanFence.h"
+
+VulkanFence::VulkanFence(std::shared_ptr<VulkanLogicalDevice> logicalDevice, const bool createInSignaledState)
+{
+    LogicalDevice = std::move(logicalDevice);
+
+    VkFenceCreateInfo fenceCreateInfo{};
+    fenceCreateInfo.sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO;
+    fenceCreateInfo.flags = createInSignaledState ? VK_FENCE_CREATE_SIGNALED_BIT : 0;
+
+    const auto result = vkCreateFence(LogicalDevice->GetRaw(), &fenceCreateInfo, nullptr, &InternalFence);
+}
+
+VulkanFence::~VulkanFence()
+{
+    if (InternalFence != VK_NULL_HANDLE)
+        vkDestroyFence(LogicalDevice->GetRaw(), InternalFence, nullptr);
+}
+
+VkFence VulkanFence::GetRaw() const
+{
+    return InternalFence;
+}
--- engine/renderer/core/VulkanFence.h
+++ engine/renderer/core/VulkanFence.h
@@ -5,9 +5,26 @@
 #ifndef CLUSEK_RT_VULKANFENCE_H
 #define CLUSEK_RT_VULKANFENCE_H
 
+#include <memory>
+#include <vulkan/vulkan.h>
+
+#include "VulkanLogicalDevice.h"
+
 class VulkanFence final
 {
+public:
+    VulkanFence(std::shared_ptr<VulkanLogicalDevice> logicalDevice, bool createInSignaledState);
+    ~VulkanFence();
+    VulkanFence(const VulkanFence& other) = delete;
+    VulkanFence(VulkanFence&& other) noexcept = delete;
+    VulkanFence& operator=(const VulkanFence& other) = delete;
+    VulkanFence& operator=(VulkanFence&& other) noexcept = delete;
 
+    [[nodiscard]] VkFence GetRaw() const;
+
+private:
+    VkFence InternalFence = VK_NULL_HANDLE;
+    std::shared_ptr<VulkanLogicalDevice> LogicalDevice;
 };
 
 
