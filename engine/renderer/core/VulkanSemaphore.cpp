08c323e2d9c725d8b5bbfb2e818867cf3b705c16
bilek993 <jbilinskipl@gmail.com>
2021-06-01 15:02:03 +0200

    Implemented Vulkan Fence.

--- engine/renderer/core/VulkanSemaphore.cpp
+++ engine/renderer/core/VulkanSemaphore.cpp
@@ -3,3 +3,26 @@
 //
 
 #include "VulkanSemaphore.h"
+
+VulkanSemaphore::VulkanSemaphore(std::shared_ptr<VulkanLogicalDevice> logicalDevice)
+{
+    LogicalDevice = std::move(logicalDevice);
+
+    VkSemaphoreCreateInfo semaphoreCreateInfo{};
+    semaphoreCreateInfo.sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO;
+
+    const auto result = vkCreateSemaphore(LogicalDevice->GetRaw(), &semaphoreCreateInfo, nullptr, &InternalSemaphore);
+    if (result != VK_SUCCESS)
+        throw std::runtime_error("Semaphore creation failed!");
+}
+
+VulkanSemaphore::~VulkanSemaphore()
+{
+    if (InternalSemaphore != VK_NULL_HANDLE)
+        vkDestroySemaphore(LogicalDevice->GetRaw(), InternalSemaphore, nullptr);
+}
+
+VkSemaphore VulkanSemaphore::GetRaw() const
+{
+    return InternalSemaphore;
+}
--- engine/renderer/core/VulkanSemaphore.h
+++ engine/renderer/core/VulkanSemaphore.h
@@ -5,10 +5,26 @@
 #ifndef CLUSEK_RT_VULKANSEMAPHORE_H
 #define CLUSEK_RT_VULKANSEMAPHORE_H
 
+#include <memory>
+#include <vulkan/vulkan.h>
+
+#include "VulkanLogicalDevice.h"
 
 class VulkanSemaphore final
 {
+public:
+    VulkanSemaphore(std::shared_ptr<VulkanLogicalDevice> logicalDevice);
+    ~VulkanSemaphore();
+    VulkanSemaphore(const VulkanSemaphore& other) = delete;
+    VulkanSemaphore(VulkanSemaphore&& other) noexcept = delete;
+    VulkanSemaphore& operator=(const VulkanSemaphore& other) = delete;
+    VulkanSemaphore& operator=(VulkanSemaphore&& other) noexcept = delete;
 
+    [[nodiscard]] VkSemaphore GetRaw() const;
+
+private:
+    VkSemaphore InternalSemaphore = VK_NULL_HANDLE;
+    std::shared_ptr<VulkanLogicalDevice> LogicalDevice;
 };
 
 
