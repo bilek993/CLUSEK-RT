da2b74d6ca20c516533815385191e942a7b83211
bilek993 <jbilinskipl@gmail.com>
2021-06-02 20:35:14 +0200

    Added empty class for vertex buffer.

---
+++ engine/renderer/core/VulkanVertexBuffer.h
@@ -0,0 +1,13 @@
+//
+// Created by jbili on 02.06.2021.
+//
+
+#ifndef CLUSEK_RT_VULKANVERTEXBUFFER_H
+#define CLUSEK_RT_VULKANVERTEXBUFFER_H
+
+class VulkanVertexBuffer final
+{
+
+};
+
+#endif //CLUSEK_RT_VULKANVERTEXBUFFER_H
