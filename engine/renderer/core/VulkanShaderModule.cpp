440ea0f3a863b6ad6e9bcb454d694dc9d2374147
bilek993 <jbilinskipl@gmail.com>
2021-06-10 15:40:12 +0200

    Added shader modules implementation.

--- engine/renderer/core/VulkanShaderModule.cpp
+++ engine/renderer/core/VulkanShaderModule.cpp
@@ -3,3 +3,68 @@
 //
 
 #include "VulkanShaderModule.h"
+
+#include <fmt/os.h>
+#include <fmt/compile.h>
+#include <fstream>
+#include <utility>
+
+VulkanShaderModule::VulkanShaderModule(std::shared_ptr<VulkanLogicalDevice> logicalDevice, const std::string& filepath)
+{
+    LogicalDevice = std::move(logicalDevice);
+
+    const auto data = ReadFile(filepath);
+    CreateInternalInstance(data);
+}
+
+VulkanShaderModule::VulkanShaderModule(std::shared_ptr<VulkanLogicalDevice> logicalDevice,
+                                       const std::vector<char>& data)
+{
+    LogicalDevice = std::move(logicalDevice);
+
+    CreateInternalInstance(data);
+}
+
+VulkanShaderModule::~VulkanShaderModule()
+{
+    if (InternalShaderModule != VK_NULL_HANDLE)
+        vkDestroyShaderModule(LogicalDevice->GetRaw(), InternalShaderModule, nullptr);
+}
+
+VkShaderModule VulkanShaderModule::GetRaw() const
+{
+    return InternalShaderModule;
+}
+
+std::vector<char> VulkanShaderModule::ReadFile(const std::string& filepath)
+{
+    std::ifstream shaderFile(filepath, std::ios::ate | std::ios::binary);
+    if (!shaderFile.is_open())
+    {
+        const auto message = fmt::format(FMT_COMPILE("Failed to open '{}'!"), filepath);
+        throw std::runtime_error(message);
+    }
+
+    auto compiledShaderSize = static_cast<size_t>(shaderFile.tellg());
+    std::vector<char> compiledShaderCode(compiledShaderSize);
+
+    shaderFile.seekg(0);
+    shaderFile.read(compiledShaderCode.data(), static_cast<std::streamsize>(compiledShaderSize));
+
+    return compiledShaderCode;
+}
+
+void VulkanShaderModule::CreateInternalInstance(const std::vector<char>& data)
+{
+    VkShaderModuleCreateInfo shaderModuleCreateInfo{};
+    shaderModuleCreateInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+    shaderModuleCreateInfo.codeSize = data.size();
+    shaderModuleCreateInfo.pCode = reinterpret_cast<const uint32_t*>(data.data());
+
+    const auto result = vkCreateShaderModule(LogicalDevice->GetRaw(),
+                                             &shaderModuleCreateInfo,
+                                             nullptr,
+                                             &InternalShaderModule);
+    if (result != VK_SUCCESS)
+        throw std::runtime_error("Shader module cannot be created!");
+}
--- engine/renderer/core/VulkanShaderModule.h
+++ engine/renderer/core/VulkanShaderModule.h
@@ -5,10 +5,31 @@
 #ifndef CLUSEK_RT_VULKANSHADERMODULE_H
 #define CLUSEK_RT_VULKANSHADERMODULE_H
 
+#include <vulkan/vulkan.h>
+#include <memory>
+
+#include "VulkanLogicalDevice.h"
 
 class VulkanShaderModule final
 {
+public:
+    VulkanShaderModule(std::shared_ptr<VulkanLogicalDevice> logicalDevice, const std::string& filepath);
+    VulkanShaderModule(std::shared_ptr<VulkanLogicalDevice> logicalDevice, const std::vector<char>& data);
+    ~VulkanShaderModule();
+    VulkanShaderModule(const VulkanShaderModule& other) = delete;
+    VulkanShaderModule(VulkanShaderModule&& other) noexcept = delete;
+    VulkanShaderModule& operator=(const VulkanShaderModule& other) = delete;
+    VulkanShaderModule& operator=(VulkanShaderModule&& other) noexcept = delete;
 
+    [[nodiscard]] VkShaderModule GetRaw() const;
+
+private:
+    std::vector<char> ReadFile(const std::string& filepath);
+    void CreateInternalInstance(const std::vector<char>& data);
+
+    VkShaderModule InternalShaderModule = VK_NULL_HANDLE;
+
+    std::shared_ptr<VulkanLogicalDevice> LogicalDevice;
 };
 
 
