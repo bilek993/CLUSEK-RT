4871c3ed9753414d13904afa468ec44d1822e028
bilek993 <jbilinskipl@gmail.com>
2021-06-10 14:54:02 +0200

    Added empty shader module class.

---
+++ engine/renderer/core/VulkanShaderModule.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jbili on 10.06.2021.
+//
+
+#include "VulkanShaderModule.h"
---
+++ engine/renderer/core/VulkanShaderModule.h
@@ -0,0 +1,15 @@
+//
+// Created by jbili on 10.06.2021.
+//
+
+#ifndef CLUSEK_RT_VULKANSHADERMODULE_H
+#define CLUSEK_RT_VULKANSHADERMODULE_H
+
+
+class VulkanShaderModule final
+{
+
+};
+
+
+#endif //CLUSEK_RT_VULKANSHADERMODULE_H
