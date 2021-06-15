ed53c1a0c2ec9f6b57d6f82129cca503821a3dc1
bilek993 <jbilinskipl@gmail.com>
2021-06-15 20:55:33 +0200

    Created empty ray tracing pipeline.

---
+++ engine/renderer/core/VulkanRayTracingPipeline.h
@@ -0,0 +1,15 @@
+//
+// Created by jbili on 15.06.2021.
+//
+
+#ifndef CLUSEK_RT_VULKANRAYTRACINGPIPELINE_H
+#define CLUSEK_RT_VULKANRAYTRACINGPIPELINE_H
+
+
+class VulkanRayTracingPipeline final
+{
+
+};
+
+
+#endif //CLUSEK_RT_VULKANRAYTRACINGPIPELINE_H
