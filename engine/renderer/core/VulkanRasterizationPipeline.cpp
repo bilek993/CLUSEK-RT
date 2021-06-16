3f7659b7d0919b41739d0eadd8cd48938f9a8585
bilek993 <jbilinskipl@gmail.com>
2021-06-16 14:01:48 +0200

    Added basic constructor and desctructor for rasterization pipeline.

--- engine/renderer/core/VulkanRasterizationPipeline.cpp
+++ engine/renderer/core/VulkanRasterizationPipeline.cpp
@@ -3,3 +3,28 @@
 //
 
 #include "VulkanRasterizationPipeline.h"
+
+VulkanRasterizationPipeline::VulkanRasterizationPipeline(std::shared_ptr<VulkanLogicalDevice> logicalDevice)
+{
+    LogicalDevice = std::move(logicalDevice);
+
+    const auto result = vkCreateGraphicsPipelines(LogicalDevice->GetRaw(),
+                                                  VK_NULL_HANDLE,
+                                                  1,
+                                                  PIPELINE_INFO_HERE,
+                                                  nullptr,
+                                                  InternalPipeline);
+    if (result != VK_SUCCESS)
+        throw std::runtime_error("Vulkan rasterization pipeline creation failed!");
+}
+
+VulkanRasterizationPipeline::~VulkanRasterizationPipeline()
+{
+    if (InternalPipeline != VK_NULL_HANDLE)
+        vkDestroyPipeline(LogicalDevice->GetRaw(), InternalPipeline, nullptr);
+}
+
+VkPipeline VulkanRasterizationPipeline::GetRaw() const
+{
+    return InternalPipeline;
+}
--- engine/renderer/core/VulkanRasterizationPipeline.h
+++ engine/renderer/core/VulkanRasterizationPipeline.h
@@ -5,10 +5,26 @@
 #ifndef CLUSEK_RT_VULKANRASTERIZATIONPIPELINE_H
 #define CLUSEK_RT_VULKANRASTERIZATIONPIPELINE_H
 
+#include <vulkan/vulkan.h>
+
+#include "VulkanLogicalDevice.h"
 
 class VulkanRasterizationPipeline final
 {
+public:
+    VulkanRasterizationPipeline(std::shared_ptr<VulkanLogicalDevice> logicalDevice);
+    ~VulkanRasterizationPipeline();
+    VulkanRasterizationPipeline(const VulkanRasterizationPipeline& other) = delete;
+    VulkanRasterizationPipeline(VulkanRasterizationPipeline&& other) noexcept = delete;
+    VulkanRasterizationPipeline& operator=(const VulkanRasterizationPipeline& other) = delete;
+    VulkanRasterizationPipeline& operator=(VulkanRasterizationPipeline&& other) noexcept = delete;
 
+    [[nodiscard]] VkPipeline GetRaw() const;
+
+private:
+    VkPipeline InternalPipeline = VK_NULL_HANDLE;
+
+    std::shared_ptr<VulkanLogicalDevice> LogicalDevice;
 };
 
 
