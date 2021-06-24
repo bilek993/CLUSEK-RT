8b172070caa52d4d854a7ec90a39a40a355b239e
bilek993 <jbilinskipl@gmail.com>
2021-06-24 18:51:31 +0200

    Added vertex macros.

--- engine/renderer/vertex/FatVertex.h
+++ engine/renderer/vertex/FatVertex.h
@@ -7,8 +7,12 @@
 
 #include <glm/glm.hpp>
 
+#include "../../common/generators/VertexMacros.h"
+
+VERTEX_STRUCTURE(FatVertex)
 struct FatVertex final
 {
+    GENERATE_ATTRIBUTE(Position, 0, 0, VK_FORMAT_R32G32B32_SFLOAT)
     glm::vec3 Position;
 };
 
