e5403e7c4389c25a85bbc9de9c79c8ae0d919231
bilek993 <jbilinskipl@gmail.com>
2021-07-06 15:00:45 +0200

    Added temporary initialization of the pipeline layout.

--- engine/renderer/core/VulkanPipelineLayout.cpp
+++ engine/renderer/core/VulkanPipelineLayout.cpp
@@ -9,6 +9,19 @@ VulkanPipelineLayout::VulkanPipelineLayout(std::shared_ptr<VulkanLogicalDevice> 
     LogicalDevice = std::move(logicalDevice);
 
     // TODO: Provide proper implementation here
+    VkPipelineLayoutCreateInfo pipelineLayoutCreateInfo{};
+    pipelineLayoutCreateInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+    pipelineLayoutCreateInfo.setLayoutCount = 0; // Optional
+    pipelineLayoutCreateInfo.pSetLayouts = nullptr; // Optional
+    pipelineLayoutCreateInfo.pushConstantRangeCount = 0; // Optional
+    pipelineLayoutCreateInfo.pPushConstantRanges = nullptr; // Optional
+
+    const auto result = vkCreatePipelineLayout(LogicalDevice->GetRaw(),
+                                               &pipelineLayoutCreateInfo,
+                                               nullptr,
+                                               &InternalPipelineLayout);
+    if (result != VK_SUCCESS)
+        throw std::runtime_error("Creating pipeline layout failed!");
 }
 
 VulkanPipelineLayout::~VulkanPipelineLayout()
