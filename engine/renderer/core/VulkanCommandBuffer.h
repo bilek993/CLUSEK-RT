e20646cc1b1bf8ba9adacd48aba95087e238d14b
bilek993 <jbilinskipl@gmail.com>
2021-07-09 10:45:01 +0200

    Set default instance count to zero when drawing.

--- engine/renderer/core/VulkanCommandBuffer.h
+++ engine/renderer/core/VulkanCommandBuffer.h
@@ -41,7 +41,7 @@ public:
     void BindPipeline(const P& pipeline, VkPipelineBindPoint bindPoint);
 
     void Draw(uint32_t indexCount,
-              uint32_t instanceCount,
+              uint32_t instanceCount = 1,
               uint32_t firstIndex = 0,
               int32_t vertexOffset = 0,
               uint32_t firstInstance = 0);
