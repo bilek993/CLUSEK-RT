9534e7efc08ff7c4266112d5780b4996ae5d45d9
bilek993 <jbilinskipl@gmail.com>
2021-07-19 15:21:01 +0200

    Added new empty vulkan class for push constant ranges.

---
+++ engine/renderer/core/VulkanPushConstantRanges.h
@@ -0,0 +1,15 @@
+//
+// Created by jbili on 19.07.2021.
+//
+
+#ifndef CLUSEK_RT_VULKANPUSHCONSTANTRANGES_H
+#define CLUSEK_RT_VULKANPUSHCONSTANTRANGES_H
+
+
+class VulkanPushConstantRanges final
+{
+
+};
+
+
+#endif //CLUSEK_RT_VULKANPUSHCONSTANTRANGES_H
